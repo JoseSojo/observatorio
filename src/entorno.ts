5ad4bb7db0c68c4d163bf75013eaf6923c694535
Jose Sojo <jsojo346@gmail.com>
2024-12-03 18:12:59 -0400

    asd

--- src/entorno.ts
+++ src/entorno.ts
@@ -1,4 +1,4 @@
 
 export const VERSION_API = `v1`;
-export const API = `http://127.0.0.1:3005/api/${VERSION_API}`;
-export const API_STATIC = `http://127.0.0.1:3005`;
+export const API = `http://3.237.86.105/api/${VERSION_API}`;
+export const API_STATIC = `http://3.237.86.105`;
